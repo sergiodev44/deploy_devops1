7d9098de6eb64e7cd5d54b76c616d83b934c0c63
SergioDawRV <slecmar974@fp.iesromerovargas.com>
2026-01-14 14:05:28 +0100

    newwww

---
+++ zugzug.py
@@ -0,0 +1,15 @@
+
+"""_summary_"""
+def suma(a: int, b: int) -> int:
+    """
+        Docstring for suma
+        
+        :param a: Description
+        :type a: int
+        :param b: Description
+        :type b: int
+        :return: Description
+        :rtype: int
+    """
+    x = "no se usa"
+    return a+ b
